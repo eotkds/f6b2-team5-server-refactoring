ffbe5e8b920d81a7a6a68bd01b3b02b458dc0edd
eotkds <eotkds@gmail.com>
2022-08-31 15:42:05 +0900

    Modify auth.service.ts Modify setheaders pop httponly, modify domain

--- backend/src/apis/auth/auth.service.ts
+++ backend/src/apis/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
     res.setHeader('Access-Control-Allow-Origin', 'https://smaf.site');
     res.setHeader(
       'Set-Cookie',
-      `refreshToken=${refreshToken}; path=/; domain=.backend.smaf.shop; SameSite=None; Secure; httpOnly;`,
+      `refreshToken=${refreshToken}; path=/; domain=.backend.smaf.site; SameSite=None; Secure; httpOnly;`,
     );
   }
 
